14bb8ff1abbec79dc5ec9819618646cfd7106ff4
Manu-Madhu <manum66466@gmail.com>
2025-07-01 00:55:18 +0530

    update the product keyword change to string

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -16,7 +16,7 @@ const ProductSchema = new Schema(
     seo: {
       title: { type: String, required: true },
       description: { type: String },
-      keywords: [{ type: String }]
+      keywords: { type: String }
     },
     description: {
       type: String
